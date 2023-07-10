2e9fe66729b8f6066273e1be3b08aef51cc6a5f9
clementfavarel <clmt.fvrl@gmail.com>
2023-07-10 04:03:04 +0200

    app finished

--- users.php
+++ users.php
@@ -31,7 +31,7 @@ $users = $stmt->fetchAll();
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
             <img src="assets/img/logo_FEDET_wo_text.png" alt="Logo FEDET" height="60">
-            <a class="navbar-brand fs-1 ms-2" href="?">FEDEstocks (beta)</a>
+            <a class="navbar-brand fs-1 ms-2" href="?">FEDEstocks</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
